f84cf16efd298e3fc139b8c87d5f32e42afa4dd9
945766782@qq.com <qinxiyun@insopur.com>
2024-10-28 16:56:58 +0800

    modify: 修改websocket 握手协议，允许通过token连接

--- ruoyi-framework/src/main/java/com/ruoyi/framework/websocket/WebSocketConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/websocket/WebSocketConfig.java
@@ -2,13 +2,23 @@ package com.ruoyi.framework.websocket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * websocket 配置
- * 
+ *
  * @author ruoyi
  */
+/*
+@Component
 @Configuration
 public class WebSocketConfig
 {
@@ -17,4 +27,41 @@ public class WebSocketConfig
     {
         return new ServerEndpointExporter();
     }
+
+
 }
+
+*/
+@Component
+@Configuration
+public class WebSocketConfig extends ServerEndpointConfig.Configurator {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+    /**
+     * 建立握手时，连接前的操作
+     */
+    @Override
+    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+        //获取请求头
+        List<String> list = request.getHeaders().get("Sec-WebSocket-Protocol");
+        //当Sec-WebSocket-Protocol请求头不为空时,需要返回给前端相同的响应
+        response.getHeaders().put("Sec-WebSocket-Protocol",list);
+        /**
+         *获取请求头后的逻辑处理
+         */
+        super.modifyHandshake(sec, request, response);
+    }
+
+    /**
+     * 初始化端点对象,也就是被@ServerEndpoint所标注的对象
+     */
+    @Override
+    public <T> T getEndpointInstance(Class<T> clazz) throws InstantiationException {
+        return super.getEndpointInstance(clazz);
+    }
+}
+
